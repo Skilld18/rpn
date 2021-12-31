be2d296121dfcb0ee7007a83a691bf7ad23669a4
Russell Dunk <russelldunk@gmail.com>
2021-12-31 10:22:54 -0500

    plus minus and parsing logic changed

--- main.py
+++ main.py
@@ -4,28 +4,49 @@ import re
 
 f = open('sample.rpn', 'r')
 
-current_symbol = ""
 
-types = [
-   ["[+-]?[0-9]+", "int"],
-   ["[+-]?[0-9]+\.[0-9]+", "real"],
-   ["\+", "plus"]
-]
-#These need to be properly greedy
+string = "\".*\""
+number = "[0-9]+"
+func = "[a-z]+"
+
+stack = [] 
+
+def plus(a,b):
+    return a + b
+
+def minus(a,b):
+    return a - b
+
+def apply(func):
+    global stack
+    arglist = []
+    for i in range(0, func.args):
+        arglist.insert(0, stack.pop())
+    stack.append(func.func(*arglist))
+    print(stack)
+
+
+def act(i):
+    global string
+    global number
+    global func
+
+    global stack
+    if re.match(string, i):
+        stack.append(i)
+    if re.match(number, i):
+        stack.append(int(i))
+    if re.match(func, i):
+        class f:
+            def __init__(self, func, args):
+                self.func = func
+                self.args = args
+        apply(f(minus, 2))
 
 opstack = []
 prog = f.read()
 for symbol in prog.split():
-    print(symbol)
-    if re.match(types[0][0], symbol):
-        print(types[0][1])
-        opstack.append(int(symbol))
-    elif re.match(types[1][0], symbol):
-        print(types[1][1])
-        opstack.append(float(symbol))
-    elif re.match(types[2][0], symbol):
-        print(types[2][1])
-        print(opstack.pop() + opstack.pop())
+    act(symbol)
 
 
 
